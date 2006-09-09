fa37634b2e06fbaca6c0b91ad6003c6bbdcd161e
ksprotte <ksprotte@2a51dca0-4d0b-0410-81ff-d9860c232a2a>
2006-09-09 08:32:03 +0000

    [ads] is getting better...
    git-svn-id: svn://common-lisp.net/project/fomus/svn/fomus/trunk@32 2a51dca0-4d0b-0410-81ff-d9860c232a2a

--- ads.lisp
+++ ads.lisp
@@ -37,10 +37,11 @@
 
 (in-package :ads)
 
-(setf *print-circle* t)	; not that I am really reading all this #1# stuff...
+(setf *print-circle* t)	; for safety - not that I am really reading all this #1# stuff...
 
-(declaim (optimize (speed 0) (safety 3) (debug 3)))
+;; (declaim (optimize (speed 0) (safety 3) (debug 3)))
 
+;;; var
 (defstruct var
   domain constraints value (tabu 0) (proj-cost most-positive-fixnum))
 
@@ -48,11 +49,15 @@
   (setf (var-value var)
 	(nth (random (length (var-domain var))) (var-domain var))))
 
-(defun var-cost-projection (var)
+(defun var-compute-proj-cost (var)
   (iter
     (for c in (var-constraints var))
     (summing (constraint-cost c))))
 
+(defun make-int-var (from to)
+  (make-var :domain (iter (for i from from to to) (collect i))))
+
+;;; constraint
 (defstruct constraint
   vars costfn
   cost			      ; when solving, the actual computed cost
@@ -81,10 +86,7 @@
 	(push c (var-constraints v)))
       c)))
 
-(defun make-int-var (from to)
-  (make-var :domain (iter (for i from from to to) (collect i))))
-
-
+;;; ads
 (defun collect-constraints (vars)
   (iter
     top
@@ -93,83 +95,123 @@
       (for c in (var-constraints v))
       (in top (adjoining c)))))
 
-(defun ads (vars &key (tabu-tenure 5))
+(defun ads (vars &key
+	    (tabu-tenure 5)
+	    (max-iterations 20)		; later this will be 100000
+	    (reset-limit (length vars)) ; number of tabu variables that will cause
+	    (reset-percentage 50)) ; a percentage of those to be randomly reset
   (let ((constraints (collect-constraints vars)))
-    (iter
-      (for v in vars)
-      (var-set-randomly v))
-    (iter
-      (for c in constraints)
-      (constraint-compute c))
+    ;; initial random config
+    (dolist (v vars) (var-set-randomly v))
+    (dolist (c constraints) (constraint-compute c))    
     ;; the big search loop
     (iter
-      (repeat 20)
-      (print (list (mapcar #'var-value vars)
-		   (iter
-		     (for c in constraints)
-		     (summing (constraint-cost c)))))
-      ;; (print (mapcar #'var-proj-cost vars))
-      ;; (print (mapcar #'var-tabu vars))
-      ;; (terpri)
-      (when (zerop (iter
-		     (for c in constraints)
-		     (summing (constraint-cost c))))
+      (with best-sol)
+      (with best-cost = most-positive-fixnum)
+      (with tabu-count = 0)
+      (repeat max-iterations)
+      (for global-cost = (iter
+			   (for c in constraints)
+			   (summing (constraint-cost c))))
+      (print (list '***var-value
+		   (mapcar #'var-value vars)
+		   'cost
+		   global-cost))
+      ;; (print (list 'tabu-count tabu-count))
+      ;;       (print (list 'var-proj-cost
+      ;; 		   (mapcar #'(lambda (x) (if (= x most-positive-fixnum) 'inf x))
+      ;; 			   (mapcar #'var-proj-cost vars))))
+      ;;       (print (list 'var-tabu (mapcar #'var-tabu vars)))
+      ;; when cost is 0 we have a perfect solution
+      (when (zerop global-cost)
+	(setf best-cost 0
+	      best-sol (mapcar #'var-value vars))
 	(terminate))
+      ;; reset
+      (when (<= reset-limit tabu-count)
+	reset-percentage		; should be used here
+	(when (< global-cost best-cost)
+	  (setf best-cost global-cost
+		best-sol (mapcar #'var-value vars)))
+	(dolist (v vars)
+	  (var-set-randomly v)
+	  (setf (var-tabu v) 0))
+	(setf tabu-count 0)
+	(dolist (c constraints) (constraint-compute c))
+	(dolist (v vars) (setf (var-proj-cost v) (var-compute-proj-cost v)))
+	(print '+resetting-vars)	
+	(print (list '***var-value
+		     (mapcar #'var-value vars)
+		     'cost
+		     (iter
+		       (for c in constraints)
+		       (summing (constraint-cost c))))))
+      ;; move
       (let ((worst-var (iter
 			 (for v in vars)
-			 (unless (zerop (var-tabu v))
-			   (decf (var-tabu v)))
-			 (when (zerop (var-tabu v))
-			   ;; (print (list 'looking 'at (var-value v)))
-			   (finding v maximizing (var-cost-projection v))))))
-	(unless worst-var (terminate))
-	;; (print (list 'found (var-value worst-var)))
+			 (cond
+			   ((< 1 (var-tabu v))
+			    ;; v is tabu and cannot be moved
+			    (decf (var-tabu v)))
+			   (t
+			    (when (= 1 (var-tabu v))
+			      ;; v was tabu but will be reactivated
+			      (decf (var-tabu v))
+			      (decf tabu-count))
+			    ;; (print (list 'looking 'at (var-value v)))
+			    (finding v maximizing (var-compute-proj-cost v)))))))
+	(unless worst-var (error "no var is movable"))
+	;; (print (list 'worst-var (var-value worst-var)))
 	(iter
 	  (with old-value = (var-value worst-var))
 	  (with proj)
 	  (for possible-move in (var-domain worst-var))
 	  (setf (var-value worst-var) possible-move)
 	  (dolist (c (var-constraints worst-var)) (constraint-compute c))
-	  (finding possible-move minimizing (setf proj (var-cost-projection worst-var))
+	  (finding possible-move minimizing (setf proj (var-compute-proj-cost worst-var))
 		   into move)
 	  (finally (cond
 		     ((< proj (var-proj-cost worst-var))
 		      (setf (var-value worst-var) move
 			    (var-proj-cost worst-var) proj
+			    ;; its tabu in the next iteration
 			    (var-tabu worst-var) 2)
-		      ;; (print (list 'moving 'to move))
+		      (incf tabu-count)
+		      (print (list 'moving 'to move))
 		      (iter
 			(for c in (var-constraints worst-var))
 			(constraint-compute c)))
 		     (t
-		      ;; (print 'tabu)
+		      (print 'tabu)
 		      (setf (var-value worst-var) old-value
 			    (var-tabu worst-var) tabu-tenure)
+		      (incf tabu-count)
 		      (dolist (c (var-constraints worst-var)) (constraint-compute c)))))))
-      (mapcar #'var-value vars))))
+      (finally (return (values best-sol best-cost))))))
 
-(let ((l (list (make-int-var 1 5)
-	       (make-int-var 1 5)
-	       (make-int-var 1 5))))
-  ;; smallest distance as possible
-  ;; between first and second
-  (post
-   '(abs (- v1 v2))
-   (first l) (second l))
-  ;; biggest distance as possible
-  ;; between second and third
-  (post
-   '(max (- 10 (abs (- v1 v2))) 0)
-   (second l) (third l))
-  ;; the entire sum should
-  ;; be greater than 10
-  (post
-   '(let ((sum (+ v1 v2 v3)))
-     (if (< 10 sum)
-	 0
-	 (- 10 sum)))
-   (first l) (second l) (third l))
-  (ads l))
+(defun example ()
+  (let ((l (list (make-int-var 1 5)
+		 (make-int-var 1 5)
+		 (make-int-var 1 5))))
+    ;; smallest distance as possible
+    ;; between first and second
+    (post
+     '(abs (- v1 v2))
+     (first l) (second l))
+    ;; biggest distance as possible
+    ;; between second and third
+    (post
+     '(max (- 10 (abs (- v1 v2))) 0)
+     (second l) (third l))
+    ;; the entire sum should
+    ;; be greater than 10
+    (post
+     '(let ((sum (+ v1 v2 v3)))
+       (if (< 10 sum)
+	   0
+	   (- 10 sum)))
+     (first l) (second l) (third l))
+    (ads l)))
 
 ;; the optimal sol is ((5 5 1) 6)
 ;; which is already reached sometimes
