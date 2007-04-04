43bf56a22c4818d81d27a14dc3cb476cb93c79a5
dpsenicka <dpsenicka@2a51dca0-4d0b-0410-81ff-d9860c232a2a>
2007-04-04 00:56:38 +0000

    ...
    git-svn-id: svn://common-lisp.net/project/fomus/svn/fomus/trunk@188 2a51dca0-4d0b-0410-81ff-d9860c232a2a

--- version.lisp
+++ version.lisp
@@ -12,7 +12,7 @@
 (declaim (type string +title+)
 	 (type cons +version+ +banner+))
 (defparameter +title+ "FOMUS")
-(defparameter +version+ '(0 2 23))
+(defparameter +version+ '(0 2 24))
 (defparameter +banner+
   `("Lisp music notation formatter"
     "Copyright (c) 2005, 2006, 2007 David Psenicka, All Rights Reserved"
